83319ec23b85e7d4baedaa09cee426c6548cdb3e
Thomas Upton <tupton@datastax.com>
2015-11-22 13:30:56 -0600

    Expand user home in profile path
    Add error handling for copying the history database so we can know if
    something like this happens in the future.

--- chrome.py
+++ chrome.py
@@ -4,15 +4,30 @@ import alfred
 import sqlite3
 import shutil
 import os
+import sys
+from time import time
+
+HISTORY_CACHE_TIME = 60
+LOCAL_HISTORY_DB = 'History'
+
+class ErrorItem(alfred.Item):
+    def __init__(self, error):
+        alfred.Item.__init__(self, {}, error.message, 'Check the workfrow log for more information.')
+
+def alfred_error(error):
+    alfred.write(alfred.xml([error]))
 
 def copy_history(profile):
-    history_file = os.path.join(profile, 'History')
+    history_file = os.path.join(os.path.expanduser(profile), 'History')
 
-    if os.path.isfile('History'):
+    if os.path.isfile(LOCAL_HISTORY_DB) and os.path.getmtime(LOCAL_HISTORY_DB) > time() - HISTORY_CACHE_TIME:
         return
 
     if os.path.isfile(history_file):
-        shutil.copy(history_file, 'History')
+        shutil.copy(history_file, LOCAL_HISTORY_DB)
+
+    if not os.path.isfile(LOCAL_HISTORY_DB):
+        raise IOError('Unable to copy Google Chrome history database from {}'.format(history_file))
 
 def get_history(query):
     conn = sqlite3.connect('History')
@@ -24,7 +39,12 @@ def get_history(query):
 if __name__ == '__main__':
     (profile, query) = alfred.args()
 
-    copy_history(profile)
+    try:
+        copy_history(profile)
+    except IOError, e:
+        alfred_error(ErrorItem(e))
+        sys.exit(-1)
+
     rows = get_history(query)
     items = [alfred.Item({'uid': row[1], 'arg': row[1]}, row[0], row[1]) for row in rows]
     xml = alfred.xml(items)
