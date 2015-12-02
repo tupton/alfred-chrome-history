ed318e5d17e60eb1c33d17c734fdd3dd3ca6e9f7
Thomas Upton <tupton@datastax.com>
2015-12-01 22:07:59 -0600

    Better sitepackages script
    * "Modularize"; extract small functions
    * Use the __name__ paradigm

--- sitepackages.py
+++ sitepackages.py
@@ -3,9 +3,22 @@ from __future__ import print_function
 import sys
 import site
 
-if hasattr(sys, 'real_prefix'):
+def is_virtual_env():
+    return hasattr(sys, 'real_prefix')
+
+def virtual_env_site_packages():
     site_packages = [p for p in sys.path if p.endswith('site-packages')]
     if len(site_packages) > 0:
-        print('{}'.format(site_packages[-1]))
-else:
-    print(site.getsitepackages()[0])
+        return '{}'.format(site_packages[-1])
+
+def site_packages():
+    return site.getsitepackages()[0]
+
+def get_site_packages():
+    if is_virtual_env():
+        return virtual_env_site_packages()
+    else:
+        return site_packages()
+
+if __name__ == '__main__':
+    print('{}'.format(get_site_packages()))
